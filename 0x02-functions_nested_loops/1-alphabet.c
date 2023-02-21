80a9693fca4e10db43a217be2c06562bf7f58085
Eng-Loay <sim.LoayIssam23106@alexu.edu.eg>
2023-02-21 14:55:00 -0800

    sad

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char cb;
+
+	for (cb = 'a'; cb <= 'z'; cb++)
+	{
+	_putchar(cb);
+	}
+	_putchar('\n');
+}
+
