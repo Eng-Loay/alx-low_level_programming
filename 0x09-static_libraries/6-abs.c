e673f39abd173e8c81ef966d6a24eb5a6073fe45
Eng-Loay <essamloay2@gmail.com>
2023-03-10 18:58:03 -0800

    first static library

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,18 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _abs - check the code for Holberton School students.
- *
- * @n: is an integer argument
- *
- * Return: Always 0.
+ * _abs - Computes the absolute value of an integer.
+ * @c: The number to be computed.
+ * Return: Absolute value of number or zero
  */
 
-int _abs(int n)
+int _abs(int c)
+
 {
-	if (n >= 0)
+
+	if (c < 0)
 	{
-		return (n);
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
 	}
-	return (-n);
+	return (c);
 }
